d3701cb403028819ec2219fe0e9afca4b43359ff
Julia Aspinall <julia.stefanczyk@gmail.com>
2017-01-07 10:51:28 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-		$link= mysqli_connect("localhost", "cl52-juliaasp", "dspw/Gqud", "cl52-juliaasp");
+		$link= mysqli_connect(add auth details here);
 		
 		if (mysqli_connect_error()) {
 			
